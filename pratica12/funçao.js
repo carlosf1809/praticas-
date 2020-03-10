4855734876fcc37ece39a0172cda5d55d28ea784
carlosf1809 <55306148+carlosf1809@users.noreply.github.com>
2020-03-10 15:42:04 -0300

    Add files via upload

---
+++ pratica12/funçao.js
@@ -0,0 +1,377 @@
+function info() {
+    if (document.getElementById("body").style.display == "") {
+        document.getElementById("body").style.display = "none";
+        document.getElementById("opacitHeader").style.opacity = "1";
+        document.getElementById("opacitSection").style.opacity = "1";
+        document.getElementById("opacitFooter").style.opacity = "1";
+
+       // document.getElementById("title").style.color = "black";
+        
+        } else {
+        document.getElementById("body").style.display = "";
+        //document.getElementById("title").style.color = "red";
+        document.getElementById("opacitHeader").style.opacity = "0.1";
+        document.getElementById("opacitSection").style.opacity = "0.1";
+        document.getElementById("opacitFooter").style.opacity = "0.1";
+        }
+        return false;
+       
+    
+}
+function fechar() {
+    if (document.getElementById("body").style.display == "none") {
+        document.getElementById("body").style.display = "";
+        document.getElementById("opacitHeader").style.opacity = "0.1";
+        document.getElementById("opacitSection").style.opacity = "0.1";
+        document.getElementById("opacitFooter").style.opacity = "0.1";
+        
+        } else {
+        document.getElementById("body").style.display = "none";
+        document.getElementById("opacitHeader").style.opacity = "1";
+        document.getElementById("opacitSection").style.opacity = "1";
+        document.getElementById("opacitFooter").style.opacity = "1";
+        }
+        return false;
+       
+    
+}
+
+function infob() {
+    if (document.getElementById("bodyb").style.display == "") {
+        document.getElementById("bodyb").style.display = "none";
+        document.getElementById("opacitHeader").style.opacity = "1";
+        document.getElementById("opacitSection").style.opacity = "1";
+        document.getElementById("opacitFooter").style.opacity = "1";
+
+       // document.getElementById("title").style.color = "black";
+        
+        } else {
+        document.getElementById("bodyb").style.display = "";
+        //document.getElementById("title").style.color = "red";
+        document.getElementById("opacitHeader").style.opacity = "0.1";
+        document.getElementById("opacitSection").style.opacity = "0.1";
+        document.getElementById("opacitFooter").style.opacity = "0.1";
+        }
+        return false;
+       
+    
+}
+function fecharb() {
+    if (document.getElementById("bodyb").style.display == "none") {
+        document.getElementById("bodyb").style.display = "";
+        document.getElementById("opacitHeader").style.opacity = "0.1";
+        document.getElementById("opacitSection").style.opacity = "0.1";
+        document.getElementById("opacitFooter").style.opacity = "0.1";
+        
+        } else {
+        document.getElementById("bodyb").style.display = "none";
+        document.getElementById("opacitHeader").style.opacity = "1";
+        document.getElementById("opacitSection").style.opacity = "1";
+        document.getElementById("opacitFooter").style.opacity = "1";
+        }
+        return false;
+       
+    
+}
+
+function infoc() {
+    if (document.getElementById("bodyc").style.display == "") {
+        document.getElementById("bodyc").style.display = "none";
+        document.getElementById("opacitHeader").style.opacity = "1";
+        document.getElementById("opacitSection").style.opacity = "1";
+        document.getElementById("opacitFooter").style.opacity = "1";
+
+       // document.getElementById("title").style.color = "black";
+        
+        } else {
+        document.getElementById("bodyc").style.display = "";
+        //document.getElementById("title").style.color = "red";
+        document.getElementById("opacitHeader").style.opacity = "0.1";
+        document.getElementById("opacitSection").style.opacity = "0.1";
+        document.getElementById("opacitFooter").style.opacity = "0.1";
+        }
+        return false;
+       
+    
+}
+function fecharc() {
+    if (document.getElementById("bodyc").style.display == "none") {
+        document.getElementById("bodyc").style.display = "";
+        document.getElementById("opacitHeader").style.opacity = "0.1";
+        document.getElementById("opacitSection").style.opacity = "0.1";
+        document.getElementById("opacitFooter").style.opacity = "0.1";
+        
+        } else {
+        document.getElementById("bodyc").style.display = "none";
+        document.getElementById("opacitHeader").style.opacity = "1";
+        document.getElementById("opacitSection").style.opacity = "1";
+        document.getElementById("opacitFooter").style.opacity = "1";
+        }
+        return false;
+       
+    
+}
+
+function infod() {
+    if (document.getElementById("bodyd").style.display == "") {
+        document.getElementById("bodyd").style.display = "none";
+        document.getElementById("opacitHeader").style.opacity = "1";
+        document.getElementById("opacitSection").style.opacity = "1";
+        document.getElementById("opacitFooter").style.opacity = "1";
+
+       // document.getElementById("title").style.color = "black";
+        
+        } else {
+        document.getElementById("bodyd").style.display = "";
+        //document.getElementById("title").style.color = "red";
+        document.getElementById("opacitHeader").style.opacity = "0.1";
+        document.getElementById("opacitSection").style.opacity = "0.1";
+        document.getElementById("opacitFooter").style.opacity = "0.1";
+        }
+        return false;
+       
+    
+}
+function fechard() {
+    if (document.getElementById("bodyd").style.display == "none") {
+        document.getElementById("bodyd").style.display = "";
+        document.getElementById("opacitHeader").style.opacity = "0.1";
+        document.getElementById("opacitSection").style.opacity = "0.1";
+        document.getElementById("opacitFooter").style.opacity = "0.1";
+        
+        } else {
+        document.getElementById("bodyd").style.display = "none";
+        document.getElementById("opacitHeader").style.opacity = "1";
+        document.getElementById("opacitSection").style.opacity = "1";
+        document.getElementById("opacitFooter").style.opacity = "1";
+        }
+        return false;
+       
+    
+}
+
+function infoe() {
+    if (document.getElementById("bodye").style.display == "") {
+        document.getElementById("bodye").style.display = "none";
+        document.getElementById("opacitHeader").style.opacity = "1";
+        document.getElementById("opacitSection").style.opacity = "1";
+        document.getElementById("opacitFooter").style.opacity = "1";
+
+       // document.getElementById("title").style.color = "black";
+        
+        } else {
+        document.getElementById("bodye").style.display = "";
+        //document.getElementById("title").style.color = "red";
+        document.getElementById("opacitHeader").style.opacity = "0.1";
+        document.getElementById("opacitSection").style.opacity = "0.1";
+        document.getElementById("opacitFooter").style.opacity = "0.1";
+        }
+        return false;
+       
+    
+}
+function fechare() {
+    if (document.getElementById("bodye").style.display == "none") {
+        document.getElementById("bodye").style.display = "";
+        document.getElementById("opacitHeader").style.opacity = "0.1";
+        document.getElementById("opacitSection").style.opacity = "0.1";
+        document.getElementById("opacitFooter").style.opacity = "0.1";
+        
+        } else {
+        document.getElementById("bodye").style.display = "none";
+        document.getElementById("opacitHeader").style.opacity = "1";
+        document.getElementById("opacitSection").style.opacity = "1";
+        document.getElementById("opacitFooter").style.opacity = "1";
+        }
+        return false;
+       
+    
+}
+
+function infof() {
+    if (document.getElementById("bodyf").style.display == "") {
+        document.getElementById("bodyf").style.display = "none";
+        document.getElementById("opacitHeader").style.opacity = "1";
+        document.getElementById("opacitSection").style.opacity = "1";
+        document.getElementById("opacitFooter").style.opacity = "1";
+
+       // document.getElementById("title").style.color = "black";
+        
+        } else {
+        document.getElementById("bodyf").style.display = "";
+        //document.getElementById("title").style.color = "red";
+        document.getElementById("opacitHeader").style.opacity = "0.1";
+        document.getElementById("opacitSection").style.opacity = "0.1";
+        document.getElementById("opacitFooter").style.opacity = "0.1";
+        }
+        return false;
+       
+    
+}
+function fecharf() {
+    if (document.getElementById("bodyf").style.display == "none") {
+        document.getElementById("bodyf").style.display = "";
+        document.getElementById("opacitHeader").style.opacity = "0.1";
+        document.getElementById("opacitSection").style.opacity = "0.1";
+        document.getElementById("opacitFooter").style.opacity = "0.1";
+        
+        } else {
+        document.getElementById("bodyf").style.display = "none";
+        document.getElementById("opacitHeader").style.opacity = "1";
+        document.getElementById("opacitSection").style.opacity = "1";
+        document.getElementById("opacitFooter").style.opacity = "1";
+        }
+        return false;
+       
+    
+}
+
+function infog() {
+    if (document.getElementById("bodyg").style.display == "") {
+        document.getElementById("bodyg").style.display = "none";
+        document.getElementById("opacitHeader").style.opacity = "1";
+        document.getElementById("opacitSection").style.opacity = "1";
+        document.getElementById("opacitFooter").style.opacity = "1";
+
+       // document.getElementById("title").style.color = "black";
+        
+        } else {
+        document.getElementById("bodyg").style.display = "";
+        //document.getElementById("title").style.color = "red";
+        document.getElementById("opacitHeader").style.opacity = "0.1";
+        document.getElementById("opacitSection").style.opacity = "0.1";
+        document.getElementById("opacitFooter").style.opacity = "0.1";
+        }
+        return false;
+       
+    
+}
+function fecharg() {
+    if (document.getElementById("bodyg").style.display == "none") {
+        document.getElementById("bodyg").style.display = "";
+        document.getElementById("opacitHeader").style.opacity = "0.1";
+        document.getElementById("opacitSection").style.opacity = "0.1";
+        document.getElementById("opacitFooter").style.opacity = "0.1";
+        
+        } else {
+        document.getElementById("bodyg").style.display = "none";
+        document.getElementById("opacitHeader").style.opacity = "1";
+        document.getElementById("opacitSection").style.opacity = "1";
+        document.getElementById("opacitFooter").style.opacity = "1";
+        }
+        return false;
+       
+    
+}
+
+function infoh() {
+    if (document.getElementById("bodyh").style.display == "") {
+        document.getElementById("bodyh").style.display = "none";
+        document.getElementById("opacitHeader").style.opacity = "1";
+        document.getElementById("opacitSection").style.opacity = "1";
+        document.getElementById("opacitFooter").style.opacity = "1";
+
+       
+        
+        } else {
+        document.getElementById("bodyh").style.display = "";
+        document.getElementById("opacitHeader").style.opacity = "0.1";
+        document.getElementById("opacitSection").style.opacity = "0.1";
+        document.getElementById("opacitFooter").style.opacity = "0.1";
+        }
+        return false;
+       
+    
+}
+function fecharh() {
+    if (document.getElementById("bodyh").style.display == "none") {
+        document.getElementById("bodyh").style.display = "";
+        document.getElementById("opacitHeader").style.opacity = "0.1";
+        document.getElementById("opacitSection").style.opacity = "0.1";
+        document.getElementById("opacitFooter").style.opacity = "0.1";
+        
+        } else {
+        document.getElementById("bodyh").style.display = "none";
+        document.getElementById("opacitHeader").style.opacity = "1";
+        document.getElementById("opacitSection").style.opacity = "1";
+        document.getElementById("opacitFooter").style.opacity = "1";
+        }
+        return false;
+       
+    
+}
+
+function infoi() {
+    if (document.getElementById("bodyi").style.display == "") {
+        document.getElementById("bodyi").style.display = "none";
+        document.getElementById("opacitHeader").style.opacity = "1";
+        document.getElementById("opacitSection").style.opacity = "1";
+        document.getElementById("opacitFooter").style.opacity = "1";
+
+       // document.getElementById("title").style.color = "black";
+        
+        } else {
+        document.getElementById("bodyi").style.display = "";
+        //document.getElementById("title").style.color = "red";
+        document.getElementById("opacitHeader").style.opacity = "0.1";
+        document.getElementById("opacitSection").style.opacity = "0.1";
+        document.getElementById("opacitFooter").style.opacity = "0.1";
+        }
+        return false;
+       
+    
+}
+function fechari() {
+    if (document.getElementById("bodyi").style.display == "none") {
+        document.getElementById("bodyi").style.display = "";
+        document.getElementById("opacitHeader").style.opacity = "0.1";
+        document.getElementById("opacitSection").style.opacity = "0.1";
+        document.getElementById("opacitFooter").style.opacity = "0.1";
+        
+        } else {
+        document.getElementById("bodyi").style.display = "none";
+        document.getElementById("opacitHeader").style.opacity = "1";
+        document.getElementById("opacitSection").style.opacity = "1";
+        document.getElementById("opacitFooter").style.opacity = "1";
+        }
+        return false;
+       
+    
+}
+    
+function infoj() {
+    if (document.getElementById("bodyj").style.display == "") {
+        document.getElementById("bodyj").style.display = "none";
+        document.getElementById("opacitHeader").style.opacity = "1";
+        document.getElementById("opacitSection").style.opacity = "1";
+        document.getElementById("opacitFooter").style.opacity = "1";
+
+       
+        
+        } else {
+        document.getElementById("bodyj").style.display = "";
+        document.getElementById("opacitHeader").style.opacity = "0.1";
+        document.getElementById("opacitSection").style.opacity = "0.1";
+        document.getElementById("opacitFooter").style.opacity = "0.1";
+        }
+        return false;
+       
+    
+}
+function fecharj() {
+    if (document.getElementById("bodyj").style.display == "none") {
+        document.getElementById("bodyj").style.display = "";
+        document.getElementById("opacitHeader").style.opacity = "0.1";
+        document.getElementById("opacitSection").style.opacity = "0.1";
+        document.getElementById("opacitFooter").style.opacity = "0.1";
+        
+        } else {
+        document.getElementById("bodyj").style.display = "none";
+        document.getElementById("opacitHeader").style.opacity = "1";
+        document.getElementById("opacitSection").style.opacity = "1";
+        document.getElementById("opacitFooter").style.opacity = "1";
+        }
+        return false;
+       
+    
+}
